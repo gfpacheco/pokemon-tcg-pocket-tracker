4699880763033e50d20134a932f7ac94ca73189c
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2025-01-03 21:22:53 -0300

    Add user card model

---
+++ prisma/migrations/20250104002141_add_user_card/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "UserCard" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "cardNumber" INTEGER NOT NULL,
+
+    CONSTRAINT "UserCard_pkey" PRIMARY KEY ("id")
+);
