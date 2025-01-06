dd2146edd9a489c0ebea21ace29d5620d9d38192
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2025-01-05 22:21:59 -0300

    Set search input type

--- src/components/search-input.tsx
+++ src/components/search-input.tsx
@@ -11,6 +11,7 @@ export function SearchInput({ value, onChange }: SearchInputProps) {
     <div className="relative w-full max-w-xs">
       <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground pointer-events-none" />
       <Input
+        type="search"
         placeholder="Search..."
         value={value}
         onChange={(event) => onChange(event.target.value)}
